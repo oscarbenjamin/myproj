45a4a24cfbe3a3c087982b4d3b07f83b0e89532d
Oscar Benjamin <enojb@bristol.ac.uk>
2018-11-08 14:45:29 +0000

    Add new plotting script

---
+++ myplot.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+def main(filename=None):
+	fig = plt.figure(figsize=(4, 3))
+	ax = fig.add_subplot(1,1,1)
+	ax.plot([1, 2, 3], [1, 2, -1])
+	ax.set_xlabel(r'$v(t) = \frac{\pi}{2}$')
+	ax.set_ylabel('time')
+	ax.set_xlim([0, 4])
+	if filename is None:
+		plt.show()
+	else:
+		fig.savefig(filename)
+
+if __name__ == "__main__":
+    import sys
+    main(*sys.argv[1:])
